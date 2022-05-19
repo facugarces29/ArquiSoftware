f73a0d1d1a291c5ad61fac24e331e861a4ed3529
lautaro <1714186@ucc.edu.ar>
2022-05-19 12:53:44 -0300

    add userclient

---
+++ client/user-client.go
@@ -0,0 +1,19 @@
+package client
+
+import (
+	"Proyecto/ArquiSoftware/model"
+
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
+
+func GetUserById(id int) model.User {
+	var user model.User
+
+	Db.Where("id = ?", id).First(&user)
+	log.Debug("User: ", user)
+
+	return user
+}
--- dto/user-dto.go
+++ dto/user-dto.go
@@ -1,6 +1,7 @@
 package dto
 
 type UserDto struct {
+	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	LastName string `json:"last_name"`
 	UserName string `json:"user_name"`
