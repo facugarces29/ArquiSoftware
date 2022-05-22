63460c946280b3b4bae3b8144bbe3924bc3dd917
lautaro <1714186@ucc.edu.ar>
2022-05-22 19:21:15 -0300

    model finished

--- model/product/product-model.go
+++ model/product/product-model.go
@@ -1,14 +1,20 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	order "Proyecto/ArquiSoftware/model/order"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Product struct {
 	gorm.Model
-	Name        string  `gorm:"type:varchar(255);not null;unique"`
-	Description string  `gorm:"type:varchar(255);not null"`
-	Price       float64 `gorm:"type:decimal(10,2);not null"`
-	Stock       int     `gorm:"type:integer(255);not null"`
-	Image       string  `gorm:"type:blob;not null"`
+	Name               string  `gorm:"type:varchar(255);not null;unique"`
+	Description        string  `gorm:"type:varchar(255);not null"`
+	Price              float64 `gorm:"type:decimal(10,2);not null"`
+	Stock              int     `gorm:"type:integer(255);not null"`
+	Image              string  `gorm:"type:blob;not null"`
+	order.OrderDetails `gorm:"foreignkey:product_id"`
+	ProductCategories `gorm:"foreignkey:product_id"`
 }
 
 type Products []Product
