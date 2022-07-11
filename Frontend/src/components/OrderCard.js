343163f61b12cc085d7cfddbfc9a15a22c745125
lautarose <1714186@ucc.edu.ar>
2022-07-11 04:42:54 -0300

    stock control finished

--- Frontend/src/components/OrderCard.js
+++ Frontend/src/components/OrderCard.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -54,14 +52,6 @@ export default function OrderCard({ image, amount, date , products , address }) 
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 }
